94c87462f548c75060f594e1946439447639bf0d
yuliakrysova <kryuma2oos@gmail.com>
2024-11-23 00:03:44 +0300

    Проверить является ли заданная строка шестизначным числом, записанным в десятичной системе счисления без нулей в старших разрядах.

--- лабораторная_работа_на_регулярные_выражения/10.py
+++ лабораторная_работа_на_регулярные_выражения/10.py
@@ -1,9 +1,11 @@
 import re
 
-number = input()
+number = ['123456', '234567', '1234567', '12345', '076745', '0000000']
 
-reg = r"^[1-9]\d+"
-if re.search(reg, number) and len(number) == 6:
-    print('YES')
-else:
-    print("NO")
+reg = r'^[1-9]\d{5}$'
+
+for i in number:
+    if re.search(reg, i):
+        print(f'Шестизначное {i}')
+    else:
+        print(f'НЕ шестизначное {i}')
