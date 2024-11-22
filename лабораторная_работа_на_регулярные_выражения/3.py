7f2ee6269a5a26843b689aeddf564f985e7a6e54
yuliakrysova <kryuma2oos@gmail.com>
2024-11-23 00:01:34 +0300

    Написать регулярное выражение, определяющее является ли заданная строка правильным MAC-адресом. – пример правильных выражений: aE:dC:cA:56:76:54. – пример неправильных выражений: 01:23:45:67:89:Az.

--- лабораторная_работа_на_регулярные_выражения/3.py
+++ лабораторная_работа_на_регулярные_выражения/3.py
@@ -1,11 +1,11 @@
 import re
 
-mac_ad = input()
+mac_ad = ["00:14:22:01:23:Z5", "aE:dC:cA:56:76:54", "01:23:45:67:89:Az", "001422-012345", "00:14:22:01:23:45", "00:00:00:00:00:00"]
 
 reg = r'([0-9A-Fa-f]{2}\:){5}([0-9A-Fa-f]{2})$'
 
-
-if re.search(reg, mac_ad) != None:
-    print('YES')
-else:
-    print('NO')
+for i in mac_ad:
+    if re.search(reg, i) != None:
+        print(f'Правильный MAC-адрес {i}')
+    else:
+        print(f'Неправильный MAC-адрес {i}')
