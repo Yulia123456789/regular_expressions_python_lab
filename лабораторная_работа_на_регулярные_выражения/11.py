58adb2aa9a856fc247418872ac09cb92ee7327df
yuliakrysova <kryuma2oos@gmail.com>
2024-11-23 00:03:59 +0300

    Есть текст со списками цен. Извлечь из него цены в USD, RUR, EU.

--- лабораторная_работа_на_регулярные_выражения/11.py
+++ лабораторная_работа_на_регулярные_выражения/11.py
@@ -1,11 +1,9 @@
 import re
 
-list_price = "Текст с ценами: 23.78 USD, 22 UDD, 53.95 EU, 0.002 USD, 00.02 US, 89.31 RUR, 65 RUR 1.9 EU 9.32 USD"
+list_price = "Текст с ценами: 23.78 USD, 22 UDD, 53.95 EU, 0.002 USD, 00.02 US, 89.31 RUR, 65 RUR, 1.9 EU, 9.32 USD, 653.95 EU, 6 RUR, 239.7 USD, 05555.033 RUR,"
 
+reg = r'(?:^|[\n\r]|[^\w\d\.])([1-9]\d*(?:.\d{,2})?\s*(?:USD|EU|RUR))\b'
 
-
-
-reg = r"([1-9][0-9]+( USD| RUR| EU))|([0-9]+\.[0-9]{1}( USD| RUR| EU){1})|([0-9]+\.[0-9]{,2}( USD| RUR| EU){1})|([0-9]\.[0-9]{,2}( USD| RUR| EU){1})"
 
 d = ([x.group() for x in re.finditer(reg, list_price)])
 
